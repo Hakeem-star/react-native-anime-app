a67e4af06326013b39a585290b91b7f2468586eb
Hakeem Ladejobi <haklad1@googlemail.com>
2022-02-01 23:30:34 +0000

    Add linear gradient to episodes

--- app/screens/twoD/Episodes.tsx
+++ app/screens/twoD/Episodes.tsx
@@ -5,8 +5,10 @@ import {
   FlatList,
   Linking,
   TouchableHighlight,
+  ImageBackground,
 } from "react-native";
 import { MediaStreamingEpisode } from "../../generated/graphql";
+import { LinearGradient } from "expo-linear-gradient";
 
 interface Props {
   streamingEpisodes?: (MediaStreamingEpisode | null)[] | null;
@@ -24,54 +26,87 @@ const Episodes = ({ streamingEpisodes, coverImageColor }: Props) => {
         contentContainerStyle={{ padding: 10 }}
         data={streamingEpisodes}
         keyExtractor={(item) => (item?.url || "") + item?.title}
-        renderItem={({ item, index }) => (
-          <TouchableHighlight
-            underlayColor="transparent"
-            onPress={() => {
-              Linking.openURL(item?.url || "");
-            }}
-          >
-            <View
-              style={{
-                padding: 15,
-                marginBottom: -2,
-                minHeight: 50,
-                borderRadius: 10,
-                borderColor: coverImageColor,
-                borderTopWidth: 2,
-                borderBottomWidth: 2,
-                borderRightWidth: index % 2 ? 0 : 2,
-                borderLeftWidth: index % 2 ? 2 : 0,
-                transform: [
-                  {
-                    translateX:
-                      index % 2
-                        ? -borderAlignmentOffset
-                        : borderAlignmentOffset,
-                  },
-                ],
-                borderTopLeftRadius: index % 2 ? 10 : 0,
-                borderBottomLeftRadius: index % 2 ? 10 : 0,
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableHighlight
+              underlayColor="transparent"
+              onPress={() => {
+                Linking.openURL(item?.url || "");
               }}
             >
-              <Text
+              <View
                 style={{
-                  // reposition the text so they are correctly aligned
+                  // overflow: "hidden",
+                  // padding: 15,
+                  marginBottom: -2,
+                  minHeight: 50,
+                  borderRadius: 10,
+                  borderColor: coverImageColor,
+                  borderTopWidth: 2,
+                  borderBottomWidth: 2,
+                  borderRightWidth: index % 2 ? 0 : 2,
+                  borderLeftWidth: index % 2 ? 2 : 0,
                   transform: [
                     {
                       translateX:
                         index % 2
-                          ? borderAlignmentOffset
-                          : -borderAlignmentOffset,
+                          ? -borderAlignmentOffset
+                          : borderAlignmentOffset,
                     },
                   ],
+                  borderTopLeftRadius: index % 2 ? 10 : 0,
+                  borderBottomLeftRadius: index % 2 ? 10 : 0,
                 }}
               >
-                {item?.title}
-              </Text>
-            </View>
-          </TouchableHighlight>
-        )}
+                <ImageBackground
+                  source={{ uri: item?.thumbnail || "" }}
+                  resizeMode="cover"
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    borderRadius: 8,
+                    overflow: "hidden",
+                    borderTopLeftRadius: index % 2 ? 8 : 0,
+                    borderBottomLeftRadius: index % 2 ? 8 : 0,
+                    borderTopRightRadius: index % 2 ? 0 : 8,
+                    borderBottomRightRadius: index % 2 ? 0 : 8,
+                  }}
+                >
+                  <LinearGradient
+                    start={{ x: index % 2 ? 0 : 1, y: 0 }}
+                    end={{ x: index % 2 ? 1 : 0, y: 0 }}
+                    colors={["rgba(255,255,255,0.2)", "rgba(255,255,255,1)"]}
+                    style={{ flex: 1 }}
+                  >
+                    <View
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "100%",
+                        padding: 15,
+
+                        // reposition the text so they are correctly aligned
+                        transform: [
+                          {
+                            translateX:
+                              index % 2
+                                ? borderAlignmentOffset
+                                : -borderAlignmentOffset,
+                          },
+                        ],
+                      }}
+                    >
+                      <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+                        {item?.title}
+                      </Text>
+                    </View>
+                  </LinearGradient>
+                </ImageBackground>
+              </View>
+            </TouchableHighlight>
+          );
+        }}
       />
     </View>
   );
